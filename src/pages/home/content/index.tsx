60ef03fb533ffe622f9a0cc45b7394d6bb6d1a16
Carlos <kaijinwooo@gmail.com>
2022-12-31 14:43:03 +0800

    ci: commitlint2

--- src/pages/home/content/index.tsx
+++ src/pages/home/content/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Carlos
  * @Date: 2022-12-30 14:41:24
- * @LastEditTime: 2022-12-30 23:53:59
+ * @LastEditTime: 2022-12-31 14:42:31
  * @FilePath: /vite-react-swc/src/pages/home/content/index.tsx
  * @Description:
  */
@@ -10,7 +10,7 @@ import Panel from '@/components/base/panel'
 import { HOME_HEADER_HEIGHT, HOME_FOOTER_HEIGHT } from '@/const'
 import { Link } from 'react-router-dom'
 
-type Props = {}
+type Props = unknown
 const NavigateList = () => (
   <ul className="py-8 px-6 bg-[#F4F6FB] bg-opacity-30 rounded-[12px]">
     <li>
