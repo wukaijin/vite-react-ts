b4ebfaa2ca191eeb48a23442e718bb4560372c38
Carlos <kaijinwooo@gmail.com>
2023-01-29 01:45:05 +0800

    ci: add images declaraton for ci

---
+++ src/images.d.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: Carlos
+ * @Date: 2023-01-29 01:43:44
+ * @LastEditTime: 2023-01-29 01:43:50
+ * @FilePath: /vite-react-swc/src/images.d.ts
+ * @Description: null
+ */
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
